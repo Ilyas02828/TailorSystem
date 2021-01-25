226aa78d486d6e9f48839ee71c2c20957216f820
ILYAS <muhammad.ilyas39504@gmail.com>
2021-01-25 15:55:30 +0500

    changing the index page

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
  */
 
 // Hi i am trying to update the github repositry through phpstorm
-
+//jlwehq;w23hwrf3;2 ihu;oi
 //,/m,/.m./
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
